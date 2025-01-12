f0fcd1cd01555ae8db2b520b9bcc66391232fea5
Oleg Broytman <phd@phdru.name>
2025-01-13 00:50:14 +0300

    Initial commit - created the project

---
+++ setup.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+setup(
+    name='mc-torrent-vfs-tests',
+    version='0.0.1',
+    description='Tests for torrent-vfs (Midnight Commander)',
+    long_description=open('README.rst', 'r').read(),
+    long_description_content_type="text/x-rst",
+    author='Oleg Broytman',
+    author_email='phd@phdru.name',
+    project_urls={
+        'Git repo': 'https://git.phdru.name/mc/mc-torrent-vfs-tests.git/',
+    },
+    license='GPL',
+    keywords=['mc', 'Midnight Commander', 'torrent', 'VFS', 'tests'],
+    platforms='Any',
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Environment :: Console',
+        'Intended Audience :: End Users/Desktop',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: 3.13',
+    ],
+    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*',
+)
