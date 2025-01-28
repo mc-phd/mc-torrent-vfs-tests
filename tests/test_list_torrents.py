71f52941653e8a4d6638b632abc81ffbcc7c82b3
Oleg Broytman <phd@phdru.name>
2025-01-28 17:21:45 +0300

    Tests: Add `test_list_torrents`
    Test `torrent list <file>.torrent`.

---
+++ tests/test_list_torrents.py
@@ -0,0 +1,45 @@
+#! /usr/bin/env python3
+
+import os
+import os.path
+import subprocess
+import sys
+
+os.environ['TZ'] = 'GMT'  # Fix timezone for date/time comparison
+
+test_dir = os.path.dirname(__file__)
+expected_dir = os.path.join(test_dir, 'data', 'expected')
+extfs_dir = os.path.join(test_dir, 'extfs.d')
+test_torrents_dir = os.path.join(test_dir, 'webtorrent-fixtures', 'fixtures')
+
+
+def _get_filename(line):
+    return line.split(None, 7)[7]
+
+
+def _test1(basename):
+    torrent_file = os.path.join(test_torrents_dir, basename + '.torrent')
+    command = os.path.join(extfs_dir, 'torrent')
+    pipe = subprocess.Popen([sys.executable, command, 'list', torrent_file],
+                            stdout=subprocess.PIPE)
+    torrent_list = pipe.stdout.read()
+    pipe.stdout.close()
+    if basename == 'corrupt':
+        assert pipe.wait() == 1  # result=1, Error
+    else:
+        assert pipe.wait() == 0  # result=0, Ok
+        torrent_list_lines = torrent_list.splitlines()
+        torrent_list_lines.sort(key=_get_filename)
+        expected_file = open(os.path.join(expected_dir, basename + '.txt'),
+                             'rb')
+        expected_lines = [line.strip() for line in expected_file.readlines()]
+        expected_file.close()
+        assert torrent_list_lines == expected_lines
+
+
+def test_list_torrents():
+    for basename in [
+        'alice', 'bunny', 'corrupt', 'folder', 'leaves-metadata',
+        'leaves', 'lots-of-numbers', 'numbers', 'sintel',
+    ]:
+        _test1(basename)
